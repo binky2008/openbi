c67c7baf9d53c6e00a6a96ebffab25e8b6c0b56d
openbusinessintelligence <openbusinessintelligence@gmail.com>
2015-03-15 17:16:20 +0100

    Java home in gradle wrapper
    Java home in gradle wrapper upgraded

--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,4 @@
-set JAVA_HOME="C:\Program Files\Java\jdk1.8.0_31"
+set JAVA_HOME="C:\Program Files\Java\jdk1.8.0_40"
 
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
